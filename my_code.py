c1f482381c4efe4a27dfbe44078c1f9b7eca1442
Ziyuan Huang <zh5959@eid.utexas.edu>
2026-03-15 22:28:32 -0500

    Add input validation to fix_phone_num; all 3 tests now passing

--- my_code.py
+++ my_code.py
@@ -1,13 +1,16 @@
 import pytest
 
+
 def fix_phone_num(phone_num_to_fix):
-  # given "5125558823". Split the parts, then recombine and return
-  area_code = phone_num_to_fix[0:3] # 512 (first three digits)
-  three_part = phone_num_to_fix[3:6] # 555 (next three digits)
-  four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
-  
-  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
-  
+  if len(phone_num_to_fix) != 10:
+    raise ValueError("Can only format numbers that are exactly 10 digits long")
+  if not phone_num_to_fix.isdigit():
+    raise ValueError("Can only format strings containing digits only")
+
+  area_code = phone_num_to_fix[0:3]
+  three_part = phone_num_to_fix[3:6]
+  four_part = phone_num_to_fix[6:]
+  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part
   return fixed_num
 
 def test_fix_phone_num():
